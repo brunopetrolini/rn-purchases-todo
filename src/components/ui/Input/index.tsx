a756089ed9fa4a617d74ff54f833887d1bba8f03
Bruno Petrolini <bruno.petrolini@outlook.com.br>
2026-07-25 11:28:07 -0300

    feat(input): add Input component with styles

---
+++ src/components/ui/Input/index.tsx
@@ -0,0 +1,9 @@
+import { TextInput, type TextInputProps } from 'react-native';
+
+import { styles } from './styles';
+
+type InputProps = TextInputProps;
+
+export function Input(props: InputProps) {
+  return <TextInput {...props} style={styles.container} />;
+}
---
+++ src/components/ui/Input/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    height: 48,
+    width: '100%',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    borderWidth: 1,
+    borderColor: '#c3c5cb',
+  },
+});
